58beb273f66d0badd158adc406d9a0d0a95ed91b
marleysidapple <bsiddhartha25@gmail.com>
2017-08-11 13:43:39 +0800

    added login view

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Login extends Component {
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<View>
+				<Text>This is login view</Text>
+			</View>
+		);
+	}
+}
+
+export default Login;
