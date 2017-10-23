0a97e298281485173f02fef88d67744b3a3a4892
marleysidapple <bsiddhartha25@gmail.com>
2017-10-23 15:51:15 +0800

    added alert component

---
+++ src/components/alerts/Alert.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Icon } from 'react-native-elements';
+
+
+class Alert extends Component {
+	constructor(props){
+		super(props);
+	}
+
+}
+
+
+export default Alert;
