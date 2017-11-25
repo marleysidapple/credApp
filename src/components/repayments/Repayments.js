1a603e647357e2d4161edf3440717e382c8e8e76
marleysidapple <bsiddhartha25@gmail.com>
2017-11-25 12:25:56 +0800

    repayment design

--- src/components/repayments/Repayments.js
+++ src/components/repayments/Repayments.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, StyleSheet, TouchableOpacity, ActivityIndicator, ListView, AsyncStorage } from 'react-native';
+import { Text, View, ScrollView, Image, StyleSheet, TouchableOpacity, ActivityIndicator, ListView, AsyncStorage } from 'react-native';
 import { Icon, ButtonGroup } from 'react-native-elements';
 import Moment from 'react-moment';
 import { FONT_NORMAL, LOAN_FONT_COLOR, FONT_SIZE } from './../../../assets/css/common';
@@ -28,15 +28,50 @@ class Repayment extends Component {
       <ScrollView style={styles.repaymentWrapper}>
         <ButtonGroup buttons={buttons} textStyle={{fontFamily: 'open-sans', fontSize: 11}} />
         <View style={styles.repaymentCell}>
+          <View style={styles.imageWrapper}>
+            <Image source={require('./../../../assets/images/repayment.png')} style={styles.repaymentIcon}  resizeMode={'contain'} />
+          </View>
 
+          <View style={styles.loanDescription}>
+              <View style={styles.description}>
+                <Text style={styles.loanTitle}>Trip To Las Vegas</Text>
+                <Text style={styles.loanContractNo}>C102909383</Text>
+              </View>
+
+              <View style={styles.repaymentAmount}>
+                  <Text>$600</Text>
+              </View>
+          </View>
         </View>
 
         <View style={styles.repaymentCell}>
-
+        <View style={styles.imageWrapper}>
+          <Image source={require('./../../../assets/images/repayment.png')} style={styles.repaymentIcon}  resizeMode={'contain'} />
+        </View>
         </View>
 
         <View style={styles.repaymentCell}>
+          <View style={styles.imageWrapper}>
+            <Image source={require('./../../../assets/images/repayment.png')} style={styles.repaymentIcon}  resizeMode={'contain'} />
+          </View>
+        </View>
 
+        <View style={styles.repaymentCell}>
+          <View style={styles.imageWrapper}>
+            <Image source={require('./../../../assets/images/repayment.png')} style={styles.repaymentIcon}  resizeMode={'contain'} />
+          </View>
+        </View>
+
+        <View style={styles.repaymentCell}>
+          <View style={styles.imageWrapper}>
+            <Image source={require('./../../../assets/images/repayment.png')} style={styles.repaymentIcon}  resizeMode={'contain'} />
+          </View>
+        </View>
+
+        <View style={styles.repaymentCell}>
+          <View style={styles.imageWrapper}>
+            <Image source={require('./../../../assets/images/repayment.png')} style={styles.repaymentIcon}  resizeMode={'contain'} />
+          </View>
         </View>
       </ScrollView>
    );
@@ -69,6 +104,30 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     borderWidth: 0,
     marginTop: 5
+  },
+
+  imageWrapper: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+
+  repaymentIcon: {
+    width: 40,
+    height: 40
+  },
+
+  loanTitle: {
+    fontFamily: 'open-sans',
+    padding: 5,
+    marginLeft: 5,
+    color: "#576068"
+  },
+
+  loanContractNo: {
+      fontFamily: 'open-sans',
+      marginLeft: 10,
+      fontSize: 10,
+      color: "#576068"
   }
 
 });
